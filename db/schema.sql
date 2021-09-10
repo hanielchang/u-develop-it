4f88010dde2a697ff405930129c7a703747091d5
hanielchang <haniellloyd@gmail.com>
2021-09-09 22:04:09 -0700

    used JOIN method in SQL

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,24 @@
+-- Notice here how the candidates table is dropped first. This is because it has a 
+-- constraint which depends upon the existence of a parties table. All dependent
+-- tables must be deleted first, then the remaining ones after.
+
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+-- Any dependent tables, such as candidates, which relies on the parties table, 
+-- should be created towards the end. Make all non-dependent tables first.
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
